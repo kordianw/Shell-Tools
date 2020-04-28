c8414be9e2fb513b7418e498877d4902364516ed
Kordian Witek <code@kordy.com>
2020-04-27 20:14:00 -0400

    initial check-in

---
+++ pr2.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+# Simple wrapper to `pr(1)' to help in displaying columns a tad nicer.
+# - it works out the screen width
+# - by default it wraps and displays text in 2 columns
+#
+# By Kordian Witek <code@kordy.com>, April 2001.
+#
+
+use strict;
+use warnings;
+
+# Default number of columns to display
+my $columns = 2;
+
+###################################################################
+
+# Work out the current screen width
+(my $screen_width = `stty size </dev/tty`) =~ s/.*\s(\d+).*/$1/s
+  or die "$0: `stty size' failed: $!\n";
+
+# How many columns to display?
+if (@ARGV)
+{
+  die "$0: a wrapper to pr(1), takes no of columns to display as parameter.\n" unless $ARGV[0] =~ /^-?\d$/;
+  $columns = abs(shift);
+}
+
+# The actual work
+open (OUT, "| pr -$columns -Tw $screen_width")
+  or die qq/$0: Can't open output to "| pr -$columns -Tw $screen_width": $!\n/;
+print OUT while (<>);
+close OUT;
+
+# EOF
