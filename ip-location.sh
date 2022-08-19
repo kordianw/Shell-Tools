d67716676c6b6508b3c0a14b4984b5b64af00a2a
Kordian Witek <code@kordy.com>
2022-08-18 22:20:18 -0400

    added new provider: ipinfo.io

--- ip-location.sh
+++ ip-location.sh
@@ -26,16 +26,34 @@ else
       #IP=`dig +short whoami.akamai.net.`
       IP=`dig +short myip.opendns.com @resolver1.opendns.com.`
     else
-      IP=`nslookup myip.opendns.com resolver1.opendns.com | awk '/Address:/{print $NF}' |tail -1`
+      IP=`nslookup myip.opendns.com resolver1.opendns.com 2>/dev/null | cat -v | awk '/Address:/{print $NF}' | sed 's/[^0-9\.]*//g' |tail -1`
+    fi
   fi
 
+  # got the IP!
   if [ -n "$IP" ]; then
     echo "* geocoding IP << $IP >>" >&2
-    curl ipinfo.io/$IP
-    curl https://tools.keycdn.com/geo.json?host=$IP
-    curl -H "User-Agent: keycdn-tools:https://google.com" https://tools.keycdn.com/geo.json?host=$IP
-    curl -w "\n" -sS http://ipinfo.io/$IP
-    #curl wtfismyip.com/json
+
+    # KEYCDN
+    echo "--> KEYCDN.COM:"
+    if which jq >&/dev/null; then
+      curl -sSL -H "User-Agent: keycdn-tools:https://google.com" http://tools.keycdn.com/geo.json?host=$IP | jq . | egrep -v '^{|^}|status"|description"|data"|geo"|host":|ip":|asn":|code":|latitude":|longitude":|metro_code"|datetime":|continent_name":|^ *}'
+    else
+      curl -w "\n" -sSL -H "User-Agent: keycdn-tools:https://google.com" http://tools.keycdn.com/geo.json?host=$IP
+    fi
+
+    # IPINFO
+    echo && echo "--> IPINFO.IO"
+    curl -sSL http://ipinfo.io/$IP |egrep -v '^{|^}|"ip":|"readme":|"loc":'
+    
+    # WTFMYISP: bonus category
+    if [ -z "$1" ]; then
+      echo && echo "--> WTFMYISP.COM:"
+      curl -sSL http://wtfismyip.com/json | egrep -v '^{|^}|TorExit":|CountryCode":|IPAddress":' | sed 's/.ucking//g'
+
+      echo && echo "--> IPAPI.CO:"
+      curl -sSL http://ipapi.co/json | egrep -v '"ip":|"version":|_code":|code_iso3":|capital":|tld":|"in_eu":|"latitude":|"longitude":|"utc_offset":|"country_calling_code":|"currency":|"currency_name":|"languages":|"country_area":|"country_population":|"asn":'
+    fi
   else
     echo "--FATAL: couldn't work out the external IP address!" >&2
   fi
